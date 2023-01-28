c545e89041558aa63dd1a842b4d8b47d64b35d8b
Kirill Larionov <strrranger1@gmail.com>
2023-01-28 18:50:03 +0300

    Add shotgun

---
+++ Assets/Script/Shoot/Devices/Arms/Shotgun.cs
@@ -0,0 +1,29 @@
+using System.Numerics;
+using NTC.Global.Pool;
+using Script.Health;
+using UnityEngine;
+using Vector3 = UnityEngine.Vector3;
+
+namespace Script.Shoot.Devices.Arms
+{
+    public class Shotgun: Weapon
+    {
+        [SerializeField] private int minBulletCount;
+        [SerializeField] private int maxBulletCount;
+        [SerializeField] private float deltaAngle;
+        
+        protected override void SpawnBullets(DamagableType[] targets)
+        {
+            var bulletCount = Random.Range(minBulletCount, maxBulletCount);
+
+            for (int i = 0; i < bulletCount; i++)
+            {
+                var angle = Random.Range(-deltaAngle, deltaAngle);
+                var newBullet = NightPool.Spawn(bullet, bulletSpawnPosition.position,
+                    bullet.transform.rotation * bulletSpawnPosition.rotation);
+                newBullet.transform.Rotate(Vector3.up, angle);
+                newBullet.Init(targets);
+            }
+        }
+    }
+}
